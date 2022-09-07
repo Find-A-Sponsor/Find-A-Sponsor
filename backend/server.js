0abc0546d50d99706cf919658b3e1abbb071b323
Uvaldo Zumaya <uvaldo.zumaya@spaceforce.mil>
2022-09-07 16:49:42 -0600

    Trial

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = require("./app")
 
 const server = https.createServer(app)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 443
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
