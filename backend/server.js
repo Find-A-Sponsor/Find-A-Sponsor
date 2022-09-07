34c4cd71b84f59dd87393856022292ab3cef061f
Uvaldo Zumaya <uvaldo.zumaya@spaceforce.mil>
2022-09-07 16:04:40 -0600

    Fixed linting errors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const app = require("./app")
 
 const server = https.createServer(app)
 
-server.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 3001
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
 })
