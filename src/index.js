03f4cace14bd7b823b74d65ff3a51dd7cc0c3c4b
Uvaldo Zumaya <uvaldo.zumaya@spaceforce.mil>
2022-09-07 17:31:43 -0600

    Trial

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { Provider } from 'react-redux';
+import App from './App';
 import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
-import usersReducer from "./frontend/reducers/usersReducer";
+/* eslint-disable import/extensions */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-shadow */
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line no-unused-vars
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import usersReducer from "./frontend/reducers/usersReducer";
 import storeInformationReducer from "./frontend/reducers/storeInformationReducer";
 import postReducer from "./frontend/reducers/postReducer";
 import storePostReducer from "./frontend/reducers/storePostReducer";
