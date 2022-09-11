3e1ecc7e4f984de6cba82a58ce0ceb603c1b4b98
Uvaldo Zumaya <uvaldo.zumaya@spaceforce.mil>
2022-09-11 03:45:01 -0600

    Added new scripts within package.json

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ const app = require("./app")
 
 const server = http.createServer(app)
 
-const PORT = process.env.PORT || 443
+const PORT = process.env.PORT || 3000
 
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./build/index.html")) // trial
+  res.sendFile(path.join(__dirname, "./build/index.html"))
 })
 
 server.listen(PORT, () => {
